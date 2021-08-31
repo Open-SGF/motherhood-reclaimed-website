fd7a01b67fe07c18a37a33324016567956340cfe
Levi Zitting <me@levizitting.com>
2021-08-30 21:52:11 -0500

    Fix vendor extraction

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,13 @@ mix.js('assets/js/main.js', 'web')
     .sass('assets/scss/main.scss', '')
     .copyDirectory('assets/static', 'web')
     .setPublicPath('web')
-    .extract(['jquery', 'badger-accordion', 'slick-carousel'])
+    .extract([
+        'jquery',
+        'badger-accordion',
+        'slick-carousel',
+        'jquery-modal',
+        'lazysizes',
+    ])
     .sourceMaps()
     .version()
     .disableSuccessNotifications()
