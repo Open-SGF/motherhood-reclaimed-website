992185727acdda03c749757d340e21032bbf1595
Ethan Zitting <ethan@ethanzitting.com>
2021-10-31 20:44:47 -0500

    integrated AWS S3 volume, updated build scripts

--- config/db.php
+++ config/db.php
@@ -10,7 +10,7 @@
 
 use craft\helpers\App;
 
-preg_match('|mysql://([a-z0-9]*):([a-z0-9]*)@([^:]*):([0-9]*)/(.*)|i', getenv('JAWSDB_URL'), $matches);
+preg_match('|mysql://([a-z0-9]*):([a-z0-9]*)@([^:]*):([0-9]*)/(.*)|i', getenv('DATABASE_URL'), $matches);
 
 $user = $matches[1];
 $password = $matches[2];
@@ -20,14 +20,14 @@ $database = $matches[5];
 
 return [
     'dsn' => App::env('DB_DSN') ?: null,
-    'driver' => "mysql",
-    'server' => $server,
-    'port' => $port,
-    'database' => $database,
-    'user' => $user,
-    'password' => $password,
-    'schema' => getenv('DB_SCHEMA'),
-    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+    'driver' => App::env('DB_DRIVER'),
+    'server' => App::env('DB_SERVER'),
+    'port' => App::env('DB_PORT'),
+    'database' => App::env('DB_DATABASE'),
+    'user' => App::env('DB_USER'),
+    'password' => App::env('DB_PASSWORD'),
+    'schema' => App::env('DB_SCHEMA'),
+    'tablePrefix' => App::env('DB_TABLE_PREFIX'),
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
 ];
