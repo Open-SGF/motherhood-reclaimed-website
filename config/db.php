a767a5fb1aa6563354f53cf0bbfd2838710abbaa
Ethan Zitting <ethan@ethanzitting.com>
2021-10-31 21:02:39 -0500

    reverted db.php per Levis advice

--- config/db.php
+++ config/db.php
@@ -10,14 +10,6 @@
 
 use craft\helpers\App;
 
-preg_match('|mysql://([a-z0-9]*):([a-z0-9]*)@([^:]*):([0-9]*)/(.*)|i', getenv('DATABASE_URL'), $matches);
-
-$user = $matches[1];
-$password = $matches[2];
-$server = $matches[3];
-$port = $matches[4];
-$database = $matches[5];
-
 return [
     'dsn' => App::env('DB_DSN') ?: null,
     'driver' => App::env('DB_DRIVER'),
