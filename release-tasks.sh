219d776cdf92881be9220094c6c78d3bfbdeaf1b
Levi Zitting <me@levizitting.com>
2023-03-12 16:54:17 -0500

    fix: issue with migrate script in release task

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,7 +3,7 @@
 echo "Running Release Tasks"
 
 echo "Running Migrations"
-php craft migrate/all --interactive=0
+php craft migrate/all --interactive=0 --no-backup
 
 echo "Applying Project Config"
 php craft project-config/apply --interactive=0
